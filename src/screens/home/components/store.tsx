5ca899dcacfda1a4f5bdef958d74c291f8b059df
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-10 10:58:15 +0700

    Stores

--- src/screens/home/components/store.tsx
+++ src/screens/home/components/store.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { FlatList, View, Text, StyleSheet } from 'react-native';
+import { FlatList, View, Text, StyleSheet, Image } from 'react-native';
 
 const Store = () => {
   const data = [
-    { id: '1', text: 'Item 1' },
-    { id: '2', text: 'Item 2' },
-    { id: '3', text: 'Item 3' },
-    { id: '4', text: 'Item 4' },
-    { id: '5', text: 'Item 5' },
+    { id: '1', text: 'สั่งพันธุ์ไทย', img: require('../../../../assets/images/stores/1.png') },
+    { id: '2', text: 'สั่งแมกซ์มาร์ท', img: require('../../../../assets/images/stores/2.png') },
+    { id: '3', text: 'สั่งน้ำมัน', img: require('../../../../assets/images/stores/3.png') },
+    { id: '4', text: 'สั่งน้ำมันฉุกเฉิน', img: require('../../../../assets/images/stores/4.png') },
+    { id: '5', text: 'Autobacs', img: require('../../../../assets/images/stores/5.png') },
   ];
 
   const renderItem = ({ item }: any) => (
     <View>
-      <View style={styles.itemIcon}></View>
+      <View style={styles.itemIcon}>
+        <Image
+          style={styles.itemImage}
+          source={item.img}
+        />
+      </View>
       <Text style={styles.itemTitle}>{item.text}</Text>
     </View>
   );
@@ -34,16 +39,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 20,
+    paddingHorizontal: 10
   },
   itemIcon: {
     backgroundColor: '#eaf7e5',
-    padding: 30,
-    marginVertical: 8,
+    width: 48,
+    height: 48,
     borderRadius: 6,
     marginHorizontal: 16,
+    padding: 5
   },
+  itemImage: {
+    width: '100%',
+    height: '100%'
+  },  
   itemTitle: {
-    textAlign: "center"
+    textAlign: "center",
+    fontFamily: "Sarabun-Regular",
+    fontSize: 12
   }
 });
 
