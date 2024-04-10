70eaac77d8ed05752d260f5f7d326347a99c07ca
iwind <134185860+iwindd@users.noreply.github.com>
2024-04-10 09:38:14 +0700

    appbar nav

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
-import { Text } from '@rneui/base';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { Col, Cols } from 'react-native-cols';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
       <View style={styles.container}>
-        <Text style={styles.title}>LOGO</Text>
+        <Cols style={styles.cols}>
+          <Col span={2}>
+            <Text style={styles.title}>LOGO</Text>
+          </Col>
+          <Col span={2} style={styles.nav}>
+            <Icon name="notifications" size={22} style={styles.menuIcon}/>
+            <Icon name="person" size={27} style={styles.menuIcon}/>
+          </Col>
+        </Cols>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  cols: {
+    marginTop: 30,
+  },
+  nav: {
+    flexDirection: 'row',
+    justifyContent: "flex-end",
+    alignItems: "center",
+    gap: 10
+  },
   container: {
     flexDirection: 'row',
     alignItems: 'center',
-    height: 70, 
+    height: 70,
     paddingHorizontal: 10,
-    backgroundColor: '#71dc47', 
+    backgroundColor: '#71dc47',
   },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginLeft: 10,
     color: 'white'
   },
   menuIcon: {
-    padding: 10,
+    color: 'white'
   },
 });
 
